b168ce4c0db0fb81943c49c8229daddd8c9319e7
Falxns <maks.falonse@gmail.com>
2021-04-13 02:33:35 +0300

    feat: implement task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let buff = str;
+  let res = '';
+  for (let i = 0; i < buff.length; i++) {
+    if (buff[i + 1] === buff[i]) {
+      let count = 0;
+      for (let j = 0; j < buff.length; j++) {
+        if (buff[j] === buff[i]) {
+          count += 1;
+        }
+      }
+      res = res.concat(`${count}${buff[i]}`);
+      buff = buff.slice(buff.lastIndexOf(buff[i]));
+      i = 0;
+    } else {
+      res = res.concat(`${buff[i]}`);
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
