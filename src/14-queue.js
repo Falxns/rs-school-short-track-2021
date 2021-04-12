f5623e6a11791594398984cdee1065b2d45795b6
Falxns <maks.falonse@gmail.com>
2021-04-13 02:54:41 +0300

    feat: implement task 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,24 @@
  */
 
 class Queue {
+  constructor() {
+    this.queue = new ListNode();
+    this.head = this.queue;
+  }
+
   get size() {
     throw new Error('Not implemented');
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  enqueue(element) {
+    this.queue.next = new ListNode(element);
+    this.queue = this.queue.next;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const buff = this.head.next.value;
+    this.head.next = this.head.next.next;
+    return buff;
   }
 }
 
